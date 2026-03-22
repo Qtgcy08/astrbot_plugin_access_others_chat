b292e1bdbd07bde926836ded44a2d5a5c8267213
KonmaKanSinPack <saierpipi@gmail.com>
2026-03-22 12:25:36 +0800

    1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
@@ -10,15 +12,40 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    @filter.llm_tool(name="access_others_chat_history") 
+    async def access_others_chat_history(self, GroupOrFriend: bool, subject_id: str,
+                                ) -> MessageEventResult:
+
+        '''访问他人聊天记录工具，
+        大模型可以调用这个工具来访问与其他id的聊天记录，调用时请按顺序确保提供正确的参数。
+        当大模型需要增加或更新全局记忆时，可以辅助这个函数来更新记忆。
+         
+
+        Args:
+            GroupOrFriend (bool): True 表示更新群记忆，False 表示更新好友记忆。
+            subject_id (str): 群id或好友id.
+        '''
+        
+        if not isinstance(GroupOrFriend, bool):
+            return "参数 GroupOrFriend 必须是布尔值，True 表示群记忆，False 表示好友记忆。"
+        type_name = "default:GroupMessage:" if GroupOrFriend else "default:FriendMessage:"
+        uid = type_name + subject_id
+        # provider_id = await self.context.get_current_chat_provider_id(uid)
+        # logger.info(f"uid:{uid}")
+
+        #获取会话历史
+        conv_mgr = self.context.conversation_manager
+        try:
+            curr_cid = await conv_mgr.get_curr_conversation_id(uid)
+            conversation = await conv_mgr.get_conversation(uid, curr_cid)  # Conversation
+        except Exception as e:
+            logger.error(f"获取会话历史失败: {e}")
+            return f"获取会话历史失败: {e}" 
+        history = json.loads(conversation.history) if conversation and conversation.history else []
+
+        return history
+        
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
