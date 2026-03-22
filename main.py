2de6aac6a15f9df158ff4a9e9b53af0277c908ff
KonmaKanSinPack <saierpipi@gmail.com>
2026-03-22 13:29:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -63,11 +63,12 @@ class MyPlugin(Star):
             if msg.get("role") in ["user", "assistant"]:
                 content_result = []
                 logger.info(f"测试1：{msg.get('content', [])}")
-                logger.info(f"测试2：{msg.get('content')[0]}")
+                # logger.info(f"测试2：{msg.get('content')[0]}")
                 for content in msg.get("content", []):
-                    logger.info(f"测试3：{content}")
-                    if content.get("type", "") == "text":
-                        content_result.append(content.get("text", ""))
+                    if isinstance(content, dict):
+                        logger.info(f"测试3：{content}")
+                        if content.get("type", "") == "text":
+                            content_result.append(content.get("text", ""))
 
                 result.append({
                     "role": msg.get("role"),
